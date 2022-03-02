47062e189905162bc4593345e893c5390e56c5ec
im mdn <ayoub.elmeo@gmail.com>
2022-03-02 14:33:30 +0100

    feat: add console log message

---
+++ index.js
@@ -0,0 +1,10 @@
+//function somme(A,B){
+//    return A+B;
+//}
+//console.log(somme(1,7))
+
+const sum = (a, b) => a + b
+console.log(sum(10, 20))
+console.log("la somme est:"+sum(10, 20))
+
+module.exports = sum;
